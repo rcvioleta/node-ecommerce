6722b6d105780f33b00ce5575cffa06fb66c667c
Rogene Cris Violeta <rcvioleta13@gmail.com>
2020-02-28 13:53:13 +0800

    Created schema to store valid password

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model('user', {
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		lowercased: true,
-		validate: (email) => {
-			const regex = /\S.\S@\S.\S/;
-			if (!regex.test(email)) {
-				throw new Error('Invalid email format!');
-			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minLength: 6
-	}
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercased: true,
+    validate: email => {
+      const regex = /\S.\S@\S.\S/;
+      if (!regex.test(email)) {
+        throw new Error("Invalid email format!");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 6
+  }
 });
 
-module.exports = User;
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+userSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+
+  return Object.keys(userObject).reduce((obj, key) => {
+    if (!["password", "tokens"].includes(key)) {
+      obj[key] = userObject[key];
+    }
+    return obj;
+  }, {});
+};
+
+module.exports = mongoose.model("user", userSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const UserController = require('../controllers/user');
+const UserController = require("../controllers/user");
 
-router.get('/users', UserController.getUsers);
-
-router.get('/user/:id', UserController.getUserById);
+router.get("/users", UserController.getUsers);
+router.get("/user/:id", UserController.getUserById);
+router.post("/user", UserController.store);
+router.patch("/user/:id", UserController.update);
+router.delete("/user/:id", UserController.delete);
 
 module.exports = router;
